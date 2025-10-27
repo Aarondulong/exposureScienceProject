538a635194dfb69645e2ff1a0b9a2db73a1174d9
Aaron Dulong <aaron_dulong@outlook.com>
2025-10-26 20:05:02 -0400

    Cleaning up code!

--- CleanIterated.py
+++ CleanIterated.py
@@ -23,23 +23,18 @@ class quantAirTools:
         for dataset in datasets: 
             
             device = pd.read_csv(dataset,delimiter=',',engine='python',on_bad_lines='skip')
-            data = pd.read_csv(dataset,delimiter=',',engine='python',skiprows=[0-2],header=3)
+            data = pd.read_csv(dataset,delimiter=',',engine='python',skiprows=[0,1,2],header=3)
             data = data[['timestamp_iso','opc_pm25','pm25_env','opc_pm10','pm10_env']]
 
             data.timestamp_iso = pd.to_datetime(data.timestamp_iso,yearfirst=True)
-            data['id'] = range(1, len(data) + 1)
-            data = data.add_suffix(f'{dataset}')
+            data = data.add_suffix(dataset)
             dataList.append(data)
             quantAirTools.dataLibrary[dataset] = data
         dataJoin = pd.concat(dataList, axis=1)
-        print(dataJoin)
         dataJoin.to_csv(outputfile)
         return outputfile
-        
-
-
-class quantAirTools:
-
+    
+    #adding plotting tool
     def QAPlotter(inputFile,startTime,endTime,var):
 
         pd.read_csv(inputFile)
